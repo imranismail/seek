acdf3a2ef1f65fe8d53c9ea810898f7687ed103a
Imran Ismail <imran.codely@gmail.com>
2017-08-09 19:03:04 +0800

    Create phoenix app without database

---
+++ apps/web/config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :web,
+  namespace: Web
+
+# Configures the endpoint
+config :web, Web.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "OeOar1lMNCqGVFmAPjk8vg4fycXSc12H3Lf1EtsRbnDgCvj0ioDSYipbw/1ojoc2",
+  render_errors: [view: Web.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Web.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+config :web, :generators,
+  context_app: false
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
