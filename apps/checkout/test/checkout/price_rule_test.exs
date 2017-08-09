2d06239d1d9f0021dd8510d97c84f3976139e409
Imran Ismail <imran.codely@gmail.com>
2017-08-09 19:21:17 +0800

    Break up test to separate cases

--- apps/checkout/test/checkout/price_rule_test.exs
+++ apps/checkout/test/checkout/price_rule_test.exs
@@ -4,9 +4,9 @@ defmodule Checkout.PriceRuleTest do
 
   alias Checkout.{Customer, PriceRule, Product}
 
-  test "PriceRule.apply_to/2" do
-    product    = Product.new(id: "classic", price: 549_99)
-    customer   = Customer.new(id: "microsoft", name: "Microsoft")
+  setup_all do
+    product  = Product.new(id: "classic", price: 549_99)
+    customer = Customer.new(id: "microsoft", name: "Microsoft")
     price_rule = PriceRule.new(
       value: -1000,
       entitled_customers: [customer],
@@ -14,18 +14,24 @@ defmodule Checkout.PriceRuleTest do
       application_method: :across
     )
 
-    checkout = Checkout.new(items: [product])
-    checkout = PriceRule.apply_to(price_rule, checkout)
+    {:ok, product: product, price_rule: price_rule, customer: customer}
+  end
+
+  test "PriceRule.apply_to/2 doesn't apply when requirements are unsatisfied", state do
+    checkout = Checkout.new(customer: :default, items: [state[:product]])
+    checkout = PriceRule.apply_to(state[:price_rule], checkout)
     checkout = Checkout.calculate_total(checkout)
 
     assert price_rule not in checkout.applied_price_rules
     assert checkout.total == product.price
+  end
 
-    checkout = Checkout.new(customer: customer, items: [product])
-    checkout = PriceRule.apply_to(price_rule, checkout)
+  test "PriceRule.apply_to/2 should apply when requirements are satisfied", state do
+    checkout = Checkout.new(customer: state[:customer], items: [state[:product]])
+    checkout = PriceRule.apply_to(state[:price_rule], checkout)
     checkout = Checkout.calculate_total(checkout)
 
-    assert price_rule in checkout.applied_price_rules
-    assert checkout.total == (product.price + price_rule.value)
+    assert state[:price_rule] in checkout.applied_price_rules
+    assert checkout.total == (state[:product].price + state[:price_rule].value)
   end
 end
