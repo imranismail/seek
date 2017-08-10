1524d01ac2ade32c50c6bf176d22dc2692079a74
Imran Ismail <imran.codely@gmail.com>
2017-08-11 03:54:30 +0800

    Update Schema macro

--- apps/checkout/lib/checkout/schema.ex
+++ apps/checkout/lib/checkout/schema.ex
@@ -4,12 +4,23 @@ defmodule Checkout.Schema do
       use Ecto.Schema
       use Checkout.Shared
 
+      import Ecto.Changeset
+      import Ecto.Query, only: [from: 2]
+
       def new(attrs \\ []) do
         struct(__MODULE__, attrs)
       end
 
+      def find(id) do
+        Checkout.Repo.get(__MODULE__, id)
+      end
+
       def find!(id) do
         Checkout.Repo.get!(__MODULE__, id)
+      end
+
+      def find_by(attrs) do
+        Checkout.Repo.get_by(__MODULE__, attrs)
       end
 
       def find_by!(attrs) do
@@ -19,6 +30,16 @@ defmodule Checkout.Schema do
       def all do
         Checkout.Repo.all(__MODULE__)
       end
+
+      defoverridable [
+        new: 1,
+        new: 0,
+        find: 1,
+        find!: 1,
+        find_by: 1,
+        find_by!: 1,
+        all: 0,
+      ]
     end
   end
 end
